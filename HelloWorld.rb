6694d00c29244e418b82b1d5fdad2d94f0f14617
BradleyGulli <bradleyjgulli@gmail.com>
2017-01-28 12:42:23 -0800

    figured out blocks a little better

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,10 +1,16 @@
 class HelloWorld
   
    def sayHello
-      puts "Hello World!"
+      yield if block_given? #this makes it so you dont always have to pass in a block
+      puts "hello again!"
    end
+
+       
 
 end
 
+
 hello = HelloWorld.new
+hello.sayHello {puts "hello world!"} # this becomes our block that happens during yield
+hello.sayHello {puts "Hello there!"} # pretty sick bro
 hello.sayHello
